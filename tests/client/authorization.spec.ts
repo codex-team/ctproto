b7e84b366d9b635d1a31b1da1f10a9aac9bf7b74
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-12-15 21:05:33 +0500

    Created specs

---
+++ tests/client/authorization.spec.ts
@@ -0,0 +1,11 @@
+describe('CTProtoClient', () => {
+  describe('SendAuthorizationMessage', () => {
+    it('should be send authorization message in 3 sec after connection', () => {
+      /**
+       * Can't implement this test yet:
+       * we didn't implement this method
+       */
+      expect(true).toBe(true);
+    });
+  });
+});
